8ee0b45a0454acb852644fc5dad13b3bfda6fc61
Art <Petrenkoartik@gmail.com>
2020-10-04 00:36:16 +0300

    Task-1 complete

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,25 @@
-const categories = document.querySelector('ul#categories');
-const categoriesChild = categories.querySelectorAll('li.item');
+const categoriesEl = document.querySelector('ul#categories');
+const categoriesChildEl = categoriesEl.querySelectorAll('li.item');
 /*функция подсчета категорий*/
-const countCategories = arr => `В списке ${arr.length} категории.`;
-console.log(countCategories(categoriesChild));
+const countCategories = arr => console.log(`В списке ${arr.length} категории.`);
+/*вызов функции*/
+countCategories(categoriesChildEl);
 
-console.log(categoriesChild.length);
+/*функция подсчета элементов внутри категории*/
+
+// categoriesChildEl.forEach(child =>
+//   console.log(
+//     `Категория: ${child.firstElementChild.textContent}
+// Количество элементов: ${child.querySelectorAll('li').length}`,
+//   ),
+// );
+
+const countItem = items =>
+  items.forEach(child =>
+    console.log(
+      `Категория: ${child.firstElementChild.textContent}
+Количество элементов: ${child.querySelectorAll('li').length}`,
+    ),
+  );
+/*вызов функции*/
+countItem(categoriesChildEl);
