fe0501187bea23265d9f16c4a14bab657d4d4b70
Art <Petrenkoartik@gmail.com>
2020-10-05 19:45:27 +0300

    task-6 complete

---
+++ js/task-6.js
@@ -0,0 +1,19 @@
+// #validation-input { border: 3px solid #bdbdbd; }
+
+// #validation-input.valid { border-color: #4caf50; }
+
+// #validation-input.invalid { border-color: #f44336; }
+
+const inputRef = document.querySelector('#validation-input');
+
+const handleInputValidChange = () => {
+  if (inputRef.value.length == inputRef.dataset.length) {
+    inputRef.classList.add('valid');
+    inputRef.classList.remove('invalid');
+  } else {
+    inputRef.classList.add('invalid');
+    inputRef.classList.remove('valid');
+  }
+};
+
+inputRef.addEventListener('change', handleInputValidChange);
